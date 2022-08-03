82bb825dfd7ab303471a0e007966022603fe1fa1
PrecisionGamingUK <Precision.Gaming@outlook.com>
2022-08-03 17:36:36 +0100

    Main Upload

---
+++ Scripting/PGSiteScript.js
@@ -0,0 +1,13 @@
+/* Mobile Nav Bar functionality - start - */
+function MobileNavBarFuntion() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+/* Mobile Nav Bar functionality - end - */
+
+
+
